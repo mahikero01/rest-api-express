f2af5b0ec360c71a7507b7d005f076a31c670711
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-12-20 08:21:57 +0800

    up

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 'use strict';
 
 var express = require('express');
+var jsonParser = require('body-parser').json;
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-    console.log("The leave on the trees are", req.query.color);
+var jsonCheck = function(req, res, next){
+    if(req.body) {
+        console.log("The sky is ", req.body.color);
+    } else {
+        console.log("There is no body property");
+    }
     next();
-});
+};
+
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
+
 
 app.listen(port, function(){
     console.log("Server is listening on Port 3000");
